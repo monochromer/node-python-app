75b5fb0c06a5677cfcf24c22417bb2803311c1f3
monochromer <monochromer@mail.ru>
2019-02-26 03:22:18 +0500

    init

---
+++ app/index.js
@@ -0,0 +1,36 @@
+const { Server } = require('http');
+const child_process = require('child_process');
+const { promisify } = require('util');
+
+const execFile = promisify(child_process.execFile);
+
+const PORT = process.env.PORT || 3000;
+
+const server = new Server();
+
+async function onRequest(req, res) {
+  const apps = [
+    ['python', ['--version']],
+    ['pip', ['--version']],
+    ['pyftsubset', ['--help']]
+  ];
+
+  Promise.all(apps.map(app => {
+    return execFile(...app).then(({ stdout, stderr }) => {
+      res.write(stdout, stderr);
+      res.write('===\n');
+    })
+  })
+  )
+  .then(() => res.end())
+  .catch(e => res.end(e))
+}
+
+server
+  .on('request', onRequest)
+  .on('error', error => {
+    console.error(error)
+  })
+  .listen(PORT, function() {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  })
